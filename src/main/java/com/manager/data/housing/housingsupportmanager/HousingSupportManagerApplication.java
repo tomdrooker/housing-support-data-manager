291f8c5d3b91e0df98325e54bc86e1e2967841a6
Tom Rooker <tomdrooker@gmail.com>
2020-04-10 16:35:45 +0100

    First commit

---
+++ src/main/java/com/manager/data/housing/housingsupportmanager/HousingSupportManagerApplication.java
@@ -0,0 +1,13 @@
+package com.manager.data.housing.housingsupportmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingSupportManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingSupportManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manager/data/housing/housingsupportmanager/HousingSupportManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.data.housing.housingsupportmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingSupportManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
