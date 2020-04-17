7a3f1ca177a34fe66e3dbe7ba121595447df7abe
Tom Rooker <tomdrooker@gmail.com>
2020-04-17 17:10:19 +0100

    Add council implemented

--- src/main/java/com/manager/data/housing/housingsupportmanager/controller/AppController.java
+++ src/main/java/com/manager/data/housing/housingsupportmanager/controller/AppController.java
@@ -1,19 +1,19 @@
 package com.manager.data.housing.housingsupportmanager.controller;
 
 import com.manager.data.housing.housingsupportmanager.model.Council;
+import com.manager.data.housing.housingsupportmanager.model.CouncilList;
 import com.manager.data.housing.housingsupportmanager.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Map;
 
 @Controller
+@SessionAttributes("councilList")
 public class AppController {
-
-    //One controller or many??
 
     @GetMapping("/home")
     public String viewHomePage(Map<String, Object> model) {
@@ -34,17 +34,58 @@ public class AppController {
         return "home";
     }
 
-    //Create council
+    // Create council
+
     @GetMapping("/add-new-council")
     public String viewAddCouncil(Model model) {
         model.addAttribute("council", new Council());
         return "add-new-council";
     }
 
+    @ModelAttribute("councilList")
+    public CouncilList createCouncilList() {
+        return new CouncilList();
+    }
+
     @PostMapping("/add-new-council")
-    public String addCouncil(@ModelAttribute(name="council") Council council) {
-        System.out.printf("%s, %s, %s", council.getCouncilName(), council.getCouncilPhone(), council.getCouncilEmail());
-        return "add-new-council"; // Send to confirmation when that page is created
+    public RedirectView create(
+            @ModelAttribute Council council,
+            @ModelAttribute("councilList") CouncilList councilList,
+            RedirectAttributes attributes) {
+        councilList.add(council);
+        attributes.addFlashAttribute("councilList", councilList);
+        return new RedirectView("./confirm-new-details");
+    }
+
+    @GetMapping("/confirm-new-details")
+    public String showForm(Model model,
+                           @ModelAttribute("councilList") CouncilList councilList) {
+        if (!councilList.isEmpty()) {
+            model.addAttribute("council", councilList.peekLast());
+        }
+        return "confirm-new-details";
+    }
+
+
+    @GetMapping("/success")
+    public String displaySuccess(Model model) {
+        return "success";
+    }
+
+    @PostMapping("/confirm-new-details")
+    public String confirmationPageRouting(@RequestParam(name = "confirmation-page-button") String button, @ModelAttribute("councilList") CouncilList councilList) {
+
+        String page = "";
+
+        if (button.equals("Submit")){
+            page = "success";
+            Council council = councilList.peekLast();
+            String councilName = council.getCouncilName();
+            String councilPhone = council.getCouncilPhone();
+            String councilEmail = council.getCouncilEmail();
+            System.out.printf("%s, %s, %s", councilName, councilPhone, councilEmail);
+        } // else if button.equals "Change details" go to the edit council bit
+        return page;
     }
 
 }
---
+++ src/main/java/com/manager/data/housing/housingsupportmanager/model/CouncilList.java
@@ -0,0 +1,6 @@
+package com.manager.data.housing.housingsupportmanager.model;
+
+import java.util.ArrayDeque;
+
+public class CouncilList extends ArrayDeque<Council> {
+}
