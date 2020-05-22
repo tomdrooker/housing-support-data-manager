00ac6c488e340d6712a5bd872623af8ff64f60bb
Tom Rooker <tomdrooker@gmail.com>
2020-05-22 12:01:17 +0100

    Cleaned up SignInController

--- src/main/java/com/manager/data/housing/housingsupportmanager/controller/SignInController.java
+++ src/main/java/com/manager/data/housing/housingsupportmanager/controller/SignInController.java
@@ -1,15 +1,9 @@
 package com.manager.data.housing.housingsupportmanager.controller;
 
-import com.manager.data.housing.housingsupportmanager.model.Council;
-import com.manager.data.housing.housingsupportmanager.model.CouncilList;
 import com.manager.data.housing.housingsupportmanager.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
-
-import java.util.Map;
 
 @Controller
 public class SignInController {
