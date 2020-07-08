db006a61cafc40cd8b684b035a4a79192be6cb20
Tom Rooker <tomdrooker@gmail.com>
2020-07-08 16:02:09 +0100

    Moved service file into its own directory

--- src/main/java/com/manager/data/housing/housingsupportmanager/controller/CouncilController.java
+++ src/main/java/com/manager/data/housing/housingsupportmanager/controller/CouncilController.java
@@ -1,6 +1,6 @@
 package com.manager.data.housing.housingsupportmanager.controller;
 
-import com.manager.data.housing.housingsupportmanager.CouncilService;
+import com.manager.data.housing.housingsupportmanager.service.CouncilService;
 import com.manager.data.housing.housingsupportmanager.model.Council;
 import com.manager.data.housing.housingsupportmanager.model.CouncilList;
 import org.springframework.beans.factory.annotation.Autowired;
